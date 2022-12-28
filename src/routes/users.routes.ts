1ea6eff9e8809be8e9f2103692faf1f09d696993
thaisalesz <thais-sales@live.com>
2022-12-28 18:49:25 -0400

    feat: hidden passwords for user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
-import { createRecruiterController } from '../controllers/recruiter.controllers'
+import { createRecruiterController, listRecruiterController } from '../controllers/recruiter.controllers'
 import { ensureEmailIsUniqueMiddleware } from '../middlewares/ensureEmailIsUnique.middleware'
     
 export const userRoutes = Router()
 
 userRoutes.post('/recruiter', ensureEmailIsUniqueMiddleware, createRecruiterController )
-userRoutes.get('/recruiter/:id')
+userRoutes.get('/recruiter/:id', listRecruiterController)
