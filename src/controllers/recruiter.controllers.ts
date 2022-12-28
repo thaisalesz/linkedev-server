1fd568b1997c8fdff69413ecb9ffcde467a0f44f
thaisalesz <thais-sales@live.com>
2022-12-28 18:29:31 -0400

    feat: rename service file

--- src/controllers/recruiter.controllers.ts
+++ src/controllers/recruiter.controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { IRecruiterRequest } from '../interfaces/recruiter'
-import { createRecruiterService } from '../services/recruiter/recruiter.services'
+import { createRecruiterService } from '../services/recruiter/createRecruiter.services'
 
 
 export const createRecruiterController = async (req:Request, res: Response) => {
