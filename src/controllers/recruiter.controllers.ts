64ffa7f5c8b034b290a7b55b3220e0f9b549c3ab
thaisalesz <thais-sales@live.com>
2022-12-28 18:46:57 -0400

    feat: list recruiter by id route

--- src/controllers/recruiter.controllers.ts
+++ src/controllers/recruiter.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { IRecruiterRequest } from '../interfaces/recruiter'
 import { createRecruiterService } from '../services/recruiter/createRecruiter.services'
+import { listRecruiterService } from '../services/recruiter/listRecruiter.service'
 
 
 export const createRecruiterController = async (req:Request, res: Response) => {
@@ -8,4 +9,11 @@ export const createRecruiterController = async (req:Request, res: Response) => {
     const newRecruiter = await createRecruiterService(recruiterData)
 
     return res.status(201).json({ data: newRecruiter })
+}
+
+export const listRecruiterController = async(req: Request, res: Response) => {
+    const recruiterId = req.params.id
+    const recruiter = await listRecruiterService(recruiterId)
+
+    return res.status(200).json( { data: recruiter })
 }
